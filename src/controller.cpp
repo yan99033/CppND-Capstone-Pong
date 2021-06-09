6f8d5e553a078a7daf327ca1f9f70943232c69b4
Shing-Yan Loo <yan99033@gmail.com>
2021-06-09 10:53:02 -0600

    feat: keyboard input signal receiver
    To enhance game play, get keyboard input from another thread
    and directly modify the game object, so that the rendering
    loop can directly render the latest game object states

--- src/controller.cpp
+++ src/controller.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include "SDL.h"
 #include "snake.h"
+#include <thread>
+#include <cassert>
 
 void Controller::ChangeDirection(Snake &snake, Snake::Direction input,
                                  Snake::Direction opposite) const {
@@ -37,5 +39,88 @@ void Controller::HandleInput(bool &running, Snake &snake) const {
           break;
       }
     }
+  }
+}
+
+template <typename T>
+T ControlSignalQueue<T>::Receive()
+{
+    // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
+    // to wait for and receive new messages and pull them from the queue using move semantics. 
+    // The received object should then be returned by the receive function. 
+//   
+  	std::unique_lock<std::mutex> lck(mutex_);
+    condition_.wait(lck, [this] { return !queue_.empty(); });
+
+    // Remove the first element in the queue (FIFO)
+    T msg = std::move(queue_.front());
+    queue_.pop_front();    
+
+    return msg; // will not be copied due to return value optimization in C++
+
+}
+
+template <typename T>
+void ControlSignalQueue<T>::Send(T &&msg)
+{
+    // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
+    // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+  
+  	std::lock_guard <std::mutex> lck(mutex_);
+    queue_.push_back(std::move(msg));
+
+    condition_.notify_one();
+}
+
+// PaddleDirection Controller2::GetLeftPaddleControl()
+// {
+
+// }
+
+Controller2::Controller2(PaddlePtr& left_paddle, PaddlePtr& right_paddle) :
+  left_paddle_(left_paddle), right_paddle_(right_paddle)
+{}
+
+/* An infinite while-loop running in a separate thread
+is used to detect key presses so that we can modify the 
+location of the paddles accordingly
+
+Get key presses without using events
+https://stackoverflow.com/questions/17580166/sdl-get-keyboard-state-without-using-events
+ */
+void Controller2::GetKeyboardInputs()
+{
+  // make sure the paddles are set
+  assert(left_paddle_ != nullptr);
+  assert(right_paddle_ != nullptr);
+
+  SDL_Event e;
+  const uint8_t* keys = SDL_GetKeyboardState(NULL);
+  while (SDL_PollEvent(&e)) {
+    if (e.type == SDL_QUIT) {
+      // exit the while-loop
+      break;
+    }
+    
+    // Update the keyboard state
+    SDL_PumpEvents();
+
+    // if both up and down keys are pressed (cancel out)
+    if (keys[SDL_SCANCODE_Q] && !keys[SDL_SCANCODE_A])
+      left_paddle_->SetDirection(PaddleDirection::Up);
+    else if (keys[SDL_SCANCODE_A] && !keys[SDL_SCANCODE_Q])
+      left_paddle_->SetDirection(PaddleDirection::Down);
+    else
+      left_paddle_->SetDirection(PaddleDirection::Stop);
+
+    if (keys[SDL_SCANCODE_P] && !keys[SDL_SCANCODE_L])
+      right_paddle_->SetDirection(PaddleDirection::Up);
+    else if (keys[SDL_SCANCODE_L] && !keys[SDL_SCANCODE_P])
+      right_paddle_->SetDirection(PaddleDirection::Down);
+    else
+      right_paddle_->SetDirection(PaddleDirection::Stop);
+
+    // Free CPU
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
   }
 }
--- src/controller.h
+++ src/controller.h
@@ -1,6 +1,13 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
+#include <deque>
+#include <mutex>
+#include <condition_variable>
+
+#include "paddle.h"
+
+
 #include "snake.h"
 
 class Controller {
@@ -11,5 +18,43 @@ class Controller {
   void ChangeDirection(Snake &snake, Snake::Direction input,
                        Snake::Direction opposite) const;
 };
+
+// class 
+template <class T>
+class ControlSignalQueue
+{
+public:
+    void Send(T &&msg);
+    T Receive();
+
+private:
+    std::deque<T> queue_;
+    std::condition_variable condition_;
+    std::mutex mutex_;
+
+};
+
+class Controller2 {
+public:
+  // Constructor
+  Controller2(PaddlePtr& left_paddle, PaddlePtr& right_paddle);
+
+  // Typical behaviour method 
+  void GetKeyboardInputs();
+  
+  
+  // PaddleDirection GetLeftPaddleControl();
+  // PaddleDirection GetRightPaddleControl();
+
+private:
+  PaddlePtr left_paddle_;
+  PaddlePtr right_paddle_;
+
+  // // keyboard control signals
+  // ControlSignalQueue<PaddleDirection> left_signal_;
+  // ControlSignalQueue<PaddleDirection> right_signal_;
+};
+
+
 
 #endif
